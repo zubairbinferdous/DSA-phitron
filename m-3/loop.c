7f2c0ff575f46c63c2e77a1829ce5a30b492f7d8
zubairbinferdous <zubidev71@gmail.com>
2023-04-14 23:43:19 +0600

    ok

---
+++ m-3/loop.c
@@ -0,0 +1,234 @@
+// for loop 
+// while loop 
+// do while loop
+
+#include<stdio.h>
+int main(){
+    // initialization 
+    // condition 
+    // increment and decrement  
+
+    // int i;
+    // for ( i = 1 ; i < 10 ; i=i+1){
+    //    printf("%d , i love you \n" , i);
+    // }
+
+    // 1 to 5 sum 
+
+    // int sum = 0;
+    // for ( int i = 1; i <= 5; i++)
+    // {
+    //     sum += i;
+    // }
+    // printf("%d", sum);
+
+    // long long int sum = 0;
+    // int i;
+    // int n;
+    // scanf("%d" , &n);
+    // for ( i=1; i <= n; i++)
+    // {
+    //     sum += i;
+    // }
+    // printf("%lld hello", sum);
+
+    // condition loop  Break
+
+    // int i;
+    // for ( i = 1; i <= 30; i++)
+    // {
+    //    if ( i % 2 == 0)
+    //    {
+    //     printf("%d-even \n", i);
+    //    }else{
+    //     printf("%d-odd \n", i);
+    //    }
+
+    //    if ( i == 15)
+    //    {
+    //     break;
+    //    }
+       
+    // }
+
+
+    // continue
+
+    // int i;
+    // for ( i = 1; i <= 30; i++)
+    // {
+
+    //   if ( i == 15)
+    //    {
+    //     continue;
+    //    }
+    //    if ( i % 2 == 0)
+    //    {
+    //     printf("%d-even \n", i);
+    //    }else{
+    //     printf("%d-odd \n", i);
+    //    }
+
+
+       
+    // 
+
+    // while loop
+
+    //  int i = 100;
+    //  while (i <=5)
+    //  {
+    //     printf("%d\n" , i );
+    //     i++;
+    //  }
+
+    //  do
+    //  {
+    //     printf("%d\n",i);
+    //     i++;
+    //  } while (i<=5);
+     
+    // problem 2 
+
+    //  int N ;
+    //  scanf("%d" , &N);
+    //  for (int i = 1; i <= N; i++)
+    //  {
+    //     printf("I Love Assignment \n");
+    //  }
+     
+    // int N;
+    // scanf("%d", &N);
+    
+    // if (N <= 0) {
+    //     for (int i = N; i <= 0; i++) {
+    //         printf("%d ", i);
+    //     }
+    // } else {
+    //     for (int i = 1; i <= N; i++) {
+    //         printf("%d ", i);
+    //     }
+    // }
+
+    // int i;
+    // int N;
+    // scanf("%d" , &N);
+    // if (N < 1)
+    // {
+    //    for ( i = N; i <= 1 ; i++)
+    //    {
+    //      printf("%d " , i);
+    //    }
+       
+    // }else{
+    //    for ( i = 1; i <= N ; i++)
+    //    {
+    //      printf("%d " , i);
+    //    }
+    // }
+
+
+    // int N, V;
+    // int sumEven = 0;
+    // int sumOdd = 0;
+    
+    // scanf("%d", &N);
+    
+    // for (int i = 0; i < N; i++) {
+    //     scanf("%d", &V);
+    //     if (V % 2 == 0) {
+    //         sumEven += V;
+    //     } else {
+    //         sumOdd += V;
+    //     }
+    // }
+    
+    // printf("%d %d", sumEven, sumOdd);
+
+    int n, x, v;
+    scanf("%d", &n);
+    
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+    
+    scanf("%d %d", &x, &v);
+    a[x] = v;
+    
+    for (int i = n - 1; i >= 0; i--) {
+        printf("%d ", a[i]);
+    }
+
+
+    // int N;
+    // int X;
+    // int V;
+    // scanf("%d", &N);
+    
+    // int A[N];
+    // for (int i = 0; i < N; i++) {
+    //     scanf("%d", &A[i]);
+    // }
+    
+    // scanf("%d %d", &X, &V);
+    // A[X] = V;
+    
+    // for (int i = N - 1; i >= 0; i--) {
+    //     printf("%d ", A[i]);
+    // }
+    
+
+
+    int N;
+    int i;
+    scanf("%d", &N);
+    
+    int A[N];
+    for ( i = 0; i < N; i++) {
+        scanf("%d", &A[i]);
+    }
+    
+    for ( i = N - 1; i >= 0; i -= 2) {
+        printf("%d ", A[i]);
+    }
+
+
+    int N;
+    scanf("%d", &N);
+    
+    int A[N];
+    for(int i = 0; i < N; i++) {
+        scanf("%d", &A[i]);
+    }
+    
+    for(int i = N - 2; i >= 0; i -= 2) {
+        printf("%d ", A[i]);
+    }
+    printf("\n");
+
+
+        int N;
+    scanf("%d", &N);
+    
+    int A[N];
+    for(int i = 0; i < N; i++) {
+        scanf("%d", &A[i]);
+    }
+    
+    for(int i = N - 1; i >= 0; i -= 2) {
+        if(i % 2 == 0) {
+            printf("%d ", A[i]);
+        }
+    }
+    printf("\n");
+     
+     
+
+
+
+
+
+    
+    return 0;
+}
---
+++ m-3/tempCodeRunnerFile.c
@@ -0,0 +1,11 @@
+    int i;
+    for ( i = 1; i <= 100; i++)
+    {
+       if ( i % 2 == 0)
+       {
+        printf("%d-even \n", i);
+       }else{
+        printf("%d-odd \n", i);
+       }
+       
+    }
